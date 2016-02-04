9f84b5111deb59ccbb1bb64e2987a6a0bba9355c
Bryan Richter <b@chreekat.net>
2016-02-04 15:51:37 -0800

    quicksave

--- src/SDMechanism.hs
+++ src/SDMechanism.hs
@@ -4,18 +4,14 @@
 module SDMechanism where
 
 import Control.Error
-import Control.Monad (forM)
 import Control.Monad.IO.Class (MonadIO)
 import Control.Monad.Reader (ReaderT)
-import Data.Maybe (fromMaybe)
 import Database.Persist
 import Database.Persist.Sql (SqlBackend)
 
 import Types
 import Persist
 
-type Mech m a = MonadIO m => ReaderT SqlBackend m a
-type EMech a = MonadIO m => ExceptT MechError (ReaderT SqlBackend m) a
 
 -- | Convert user's value to a MechPatron
 importPatron :: ToMechPatron a => a -> EMech (Entity MechPatron)
@@ -25,39 +21,53 @@ importPatron = (!? NoSuchPatron) . getBy . ExternalPatron . mechPatron
 importProject :: ToMechProject a => a -> EMech (Entity MechProject)
 importProject = (!? NoSuchProject) . getBy . ExternalProject . mechProject
 
+-- *** Some conveniences (internal)
 right :: Monad m => m a -> ExceptT e m a
 right = ExceptT . fmap Right
 
-newPledge :: (ToMechPatron pat, ToMechProject pro)
-          => pro -> pat -> Mech m (Either MechError ())
+type Mech a = forall m. MonadIO m => ReaderT SqlBackend m a
+type EMech a = forall m. MonadIO m => ExceptT MechError (ReaderT SqlBackend m) a
+
+-- | Register a new pledge. Currently throws an error if the pledge already
+-- exists; maybe better to ignore it quietly?
+newPledge :: (ToMechProject pro, ToMechPatron pat)
+          => pro -> pat -> Mech (Either MechError ())
 newPledge r a = runExceptT $ do
-    Entity proId pro <- importProject r
+    Entity proId _   <- importProject r
     Entity patId pat <- importPatron a
-    _ <- nonExistent (UniquePledge proId patId) !? ExistingPledge
+    assertNonExistentPledge proId patId
     otherPatrons <- right $ count [PledgeProject ==. proId]
     case compare (mechPatronWallet pat) otherPatrons of
         LT -> throwE InsufficientFunds
         _  -> right $ insert_ (Pledge proId patId)
+  where
+    -- | Throw ExistingPledge if called for.
+    assertNonExistentPledge pro pat = (!? ExistingPledge) $
+        maybe (Just ()) (const Nothing) <$> getBy (UniquePledge pro pat)
 
-nonExistent :: ( PersistEntity val
-               , MonadIO m
-               , PersistUnique (PersistEntityBackend val))
-            => Unique val -> ReaderT (PersistEntityBackend val) m (Maybe ())
-nonExistent a = do
-    a' <- getBy a
-    return $ case a' of
-        Nothing -> Just ()
-        Just _ -> Nothing
+-- | Delete a pledge. Currently ignores nonexistent pledges quietly,
+-- because that's the Persistent default for deleteBy, but complains about
+-- nonexistent patrons and projects. Maybe it should ignore their absence
+-- quietly?
+deletePledge :: (ToMechProject pro, ToMechPatron pat)
+             => pro -> pat -> Mech (Either MechError ())
+deletePledge r a = runExceptT $ do
+    pro <- entityKey <$> importProject r
+    pat <- entityKey <$> importPatron a
+    right (deleteBy (UniquePledge pro pat))
 
--- deletePledge :: (ToMechPatron pat, ToMechProject pro)
---              => pro-> pat -> Mech m ()
--- deletePledge r a = exceptT return return $ do
---     let pro = importProject r
---         pat = importPatron a
---     right (deleteBy (UniquePledge pro pat))
+patronPledges :: (ToMechProject pro, ToMechPatron pat)
+              => pat -> Mech (Either MechError [pro])
+patronPledges a = runExceptT $ do
+    pat <- entityKey <$> importPatron a
+    right $ do
+        pros <- fmap (map (pledgeProject . entityVal))
+                     (selectList [PledgePatron ==. pat] [])
+        fmap (map extractProject)
+             (selectList [MechProjectId <-. pros] [])
+  where
+    extractProject = toExternalProject . mechProjectExternalKey . entityVal
 
--- tellPledges :: (ToMechProject pro, ToMechPatron pat) => pro -> Mech m [pat]
--- tellPledges a = fmap (map export) (selectList [PledgeProject ==. (ma a)] [])
 --   where export = toPatron . pledgePatron . entityVal
 --
 
